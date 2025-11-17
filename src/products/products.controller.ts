39446592ae6f900542d030d175907710c442b192
Janitha Ranasinghe <janitha479@gmail.com>
2025-11-17 16:46:11 +0530

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -30,6 +30,8 @@ export class ProductsController {
   update(@Param('id') id: string, @Body() dat) {
     return this.productsService.update(+id, dat);
   }
+
+  //this is delete method
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
